d8975519a56e1be3ec1751d9010df46ab77db8b3
IdealistCat <113909588+IdealistCat@users.noreply.github.com>
2024-11-10 12:17:43 -0600

    Ghost Tapping

--- source/gameplay/PlayState.hx
+++ source/gameplay/PlayState.hx
@@ -2068,15 +2068,6 @@ class PlayState extends MusicBeatState
 		var rightP = controls.RIGHT_P;
 		var downP = controls.DOWN_P;
 		var leftP = controls.LEFT_P;
-
-		if (leftP)
-			noteMiss(0);
-		if (downP)
-			noteMiss(1);
-		if (upP)
-			noteMiss(2);
-		if (rightP)
-			noteMiss(3);
 	}
 
 	function noteCheck(keyP:Bool, note:Note):Void
