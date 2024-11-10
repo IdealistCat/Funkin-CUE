308c714735794561dd018fad15451be8528ee2f4
IdealistCat <113909588+IdealistCat@users.noreply.github.com>
2024-11-10 10:33:23 -0600

    typo fix

--- source/utilities/AssetPaths.hx
+++ source/utilities/AssetPaths.hx
@@ -5,6 +5,6 @@ class AssetPaths
     public static var DATA_FOLDER = "assets/data";
     public static var FONT_FOLDER = "assets/fonts";
     public static var IMAGE_FOLDER = "assets/images";
-    public static var SOUND_FOLDER = "assets/sound";
+    public static var SOUND_FOLDER = "assets/sounds";
     public static var MUSIC_FOLDER = "assets/music";
 }
