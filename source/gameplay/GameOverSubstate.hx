f3029b0c4f90d7cd692b1b44b7f205e2c3b38abd
IdealistCat <113909588+IdealistCat@users.noreply.github.com>
2024-11-10 10:32:50 -0600

    type not found

--- source/gameplay/GameOverSubstate.hx
+++ source/gameplay/GameOverSubstate.hx
@@ -6,6 +6,7 @@ import flixel.FlxSubState;
 import flixel.math.FlxPoint;
 import flixel.util.FlxColor;
 import flixel.util.FlxTimer;
+import utilities.AssetPaths;
 
 class GameOverSubstate extends MusicBeatSubstate
 {
