2c357585033f991656a842a891fe08c5e8dcaf13
IdealistCat <113909588+IdealistCat@users.noreply.github.com>
2024-11-10 13:40:36 -0600

    crash handler

--- source/Main.hx
+++ source/Main.hx
@@ -1,5 +1,6 @@
 package;
 
+import openfl.Assets;
 import crashdumper.SessionData;
 import crashdumper.CrashDumper;
 import flixel.FlxGame;
@@ -17,8 +18,15 @@ class Main extends Sprite
 		// generates unique id: "FNFCUE_YYYY-MM-DD_HH'MM'SS_CRASH"
 
 		var crashDumper = new CrashDumper(unique_id);
-		crashDumper.set_path('./crashdumper');
 		// starts the crashDumper
+
+		//Here is where you would load your config and/or save data from file
+		//(in this example, we just grab a fake config.xml from assets, 
+		//but you should load them from wherever your app stores them)
+		#if (windows || mac || linux)
+			var fakeConfigFile:String = Assets.getText("assets/config.xml");
+			crashDumper.session.files.set("config.xml", fakeConfigFile);
+		#end
 
 		#if !mobile
 		addChild(new FPS(10, 3, 0xFFFFFF));
--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -84,7 +84,7 @@ class MainMenuState extends MusicBeatState
 
 		FlxG.camera.follow(camFollow, null, 0.06);
 
-		var versionShit:FlxText = new FlxText(0, 0, 0, "Mod Base: Funkin v0.2.7.1 ( 50fccde | master | modified )\nFunkin CUE v" + Application.current.meta.get('version'), 12);
+		var versionShit:FlxText = new FlxText(0, 0, 0, "Funkin v0.2.7.1 ( commit 50fccde )\nFunkin CUE v" + Application.current.meta.get('version'), 12);
 		versionShit.scrollFactor.set();
 		versionShit.x = 8;
 		versionShit.y = FlxG.height - versionShit.height - 4;
