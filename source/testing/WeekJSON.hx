02476abbba3d2c1817644bf920938d40baee66bd
IdealistCat <113909588+IdealistCat@users.noreply.github.com>
2024-11-11 18:54:17 -0600

    demotivation

--- source/testing/WeekJSON.hx
+++ source/testing/WeekJSON.hx
@@ -1,8 +1,9 @@
 package testing;
 
+import haxe.Json;
 import lime.utils.Assets;
 import utilities.AssetPaths;
-import data.jsons.Week.WeekJSON;
+import data.jsons.Week.WeekJSON as WJ;
 import data.jsons.Week.WeekJSONmanager;
 import flixel.text.FlxText;
 
@@ -14,8 +15,8 @@ class WeekJSON extends MusicBeatState {
         var curpath:String = '${AssetPaths.WEEK_DATA_FOLDER}/week1.json';
 		var err:String = '"$curpath"'+" doesn't exist";
         try{
-            var songJSON = Assets.getText('$curpath');
-            data.text = songJSON;
+            var songJSON:WJ = Json.parse(Assets.getText('$curpath'));
+            data.text = songJSON.list[0];
         } catch(e) {
             trace(err);
             data.text = err;
